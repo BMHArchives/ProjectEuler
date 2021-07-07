0cee17ade7043107744ab3e6e7c206d3a967d516
Brandon M Hunter <brandonmichaelhunter@gmail.com>
2021-07-07 17:32:31 -0400

    Removed num py reference
    Signed-off-by: Brandon M Hunter <brandonmichaelhunter@gmail.com>

--- Problem_8/Problem_8.py
+++ Problem_8/Problem_8.py
@@ -24,7 +24,7 @@
 
 # Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 from functools import reduce
-import numpy as np
+
 text = "73167176531330624919225119674426574742355349194934" \
 "96983520312774506326239578318016984801869478851843" \
 "85861560789112949495459501737958331952853208805511" \
